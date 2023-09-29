2cade5ebb819c3324dc835629688790ac3a7ad7f
Murilo <mbalves1@outlook.com>
2023-09-29 20:16:57 -0300

    [feat]: add dockerfile and prisma

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,59 @@
 const express = require("express")
 
-const allTodos = [{name: "Murilo", status: false}]
 const allRoutes = express.Router()
+const { PrismaClient } = require("@prisma/client")
 
-allRoutes.post("/all", (req, res) => {
+const prisma = new PrismaClient()
+
+allRoutes.post("/all", async (req, res) => {
   const { name } = req.body
-  allTodos.push({ name, status: false })
-  console.log(req);
-  return res.status(201).json(allTodos)
+  const todo = await prisma.todo.create({
+    data: {
+      name
+    }
+  })
+  return res.status(201).json(todo)
 })
 
-allRoutes.get("/all", (req, res) => {
-  return res.status(200).json(allTodos)
+allRoutes.get("/all", async (req, res) => {
+  const todo = await prisma.todo.findMany()
+  return res.status(200).json(todo)
 })
+
+allRoutes.put("/all", async (req, res) => {
+  const {name, id, status} = req.body
+
+  const todo = await prisma.todo.update({
+    where: {
+      id
+    },
+    data: {
+      name, status
+    }
+  })
+  return res.status(200).json(todo);
+})
+
+allRoutes.delete("/all/:id", async (request, response) => {
+  const { id } = request.params;
+
+  const intId = parseInt(id);
+
+  if (!intId) {
+    return response.status(400).json("Id is mandatory");
+  }
+
+  const todoAlreadyExist = await prisma.todo.findUnique({
+    where: { id: intId },
+  });
+
+  if (!todoAlreadyExist) {
+    return response.status(404).json("Todo not exist");
+  }
+
+  await prisma.todo.delete({ where: { id: intId } });
+
+  return response.status(200).send();
+});
 
 module.exports = allRoutes
